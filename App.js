6880f1cf4f16ee2f90fa36226df669488aaea7bd
Chansen17 <Chansenwebdev@gmail.com>
2018-11-17 19:17:55 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        show: false,
+        favorite: false,
+        wonders: [
+            {name: 'Machu Picchu, Peru', url: 'https://images.unsplash.com/photo-1461681922067-669418071e5c?auto=format&fit=crop&w=1650&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
+            {name: 'The Roman Colosseum, Italy', url: 'https://images.unsplash.com/photo-1460722665083-c2599113f7e0?auto=format&fit=crop&w=1650&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
+            {name: 'Ancient City, Petra Jordan', url: 'https://images.unsplash.com/photo-1501232060322-aa87215ab531?auto=format&fit=crop&w=1655&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
+            {name: 'Chichen Itza, Mexico', url: 'https://images.unsplash.com/photo-1503187680590-525b6e7a793f?auto=format&fit=crop&w=1332&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
+            {name: 'Christ Redeemer, Brazil', url: 'https://images.unsplash.com/photo-1510333337682-fdd0eba357a4?auto=format&fit=crop&w=750&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
+            {name: 'Great Wall, China', url: 'https://images.unsplash.com/photo-1461681922067-669418071e5c?auto=format&fit=crop&w=1650&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
+            {name: 'Taj Mahal, India', url: 'https://images.unsplash.com/photo-1461681922067-669418071e5c?auto=format&fit=crop&w=1650&q=80', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'}
+        ]
+    },
+    methods: {
+        
+    }
+});
